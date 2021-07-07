cc4b9df717a922038d1deed8237a4b94ee37ea5b
Raul Zaldana-Calles <raul.zaldana@irdeto.com>
2021-07-06 22:55:16 -0400

    Add emission shader to object in addObjectWithModifiers.py

--- addObjectWithModifiers.py
+++ addObjectWithModifiers.py
@@ -42,3 +42,12 @@ so.data.materials.append(new_mat)
 new_mat.use_nodes = True
 nodes = new_mat.node_tree.nodes
 
+material_output = nodes.get("Material Output")
+
+node_emission = nodes.new(type='ShaderNodeEmission')
+
+node_emission.inputs[0].default_value = (0.0, 0.3, 1.0, 1) #color
+node_emission.inputs[1].default_value = 500.0 #strenght
+
+links = new_mat.node_tree.links
+new_link = links.new(node_emission.outputs[0], material_output.inputs[0])
